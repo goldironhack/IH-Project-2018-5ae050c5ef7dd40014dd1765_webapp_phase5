727a3e8062bdfcfe029507bfb10c980647983d28
Bernice-Li <lix1994@126.com>
2018-05-29 23:41:36 -0400

    Automatic commit. Done before go to queltrics

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ## webapp_phase5
-
-undefined
+name : "room searching"
+key words: distance,safety,affordability.
+expected function：accordding to data already know to filter suitable districts for reference.
+test browser: firefox
+problem: i could not match the borough name dataset and the geoshapes dataset. 
--- index.js
+++ index.js
@@ -1,6 +1,59 @@
+ window.myname = function(results){
+          for (var i = 0; i < results.data.length; i++) {
+            var arr =  results.data[i];        
+        var strll = arr[9]
+        console.log(strll)
+           var n1 = strll.substr(7,18)
+           var n2 = strll.substr(25,18)
+           n2 = n2.replace(")","")        
+           var namell =[n1,n2];
+            }
+          }
 
-(function () {
-    document.getElementById("btn").addEventListener("click", function () {
-        alert("Hi! :)");
-    });
-})();
+      window.mycrime = function(results){
+          for (var i = 0; i < results.data.length; i++) {
+            var crr1=  results.data[i][31];
+            var crr2=  results.data[i][32];      
+        function crrCheck(crr){
+            var newCrr = [];
+          for(var i=0;i<crr.length;i++){
+            var temp=crr[i];
+            var count=0;
+            for(var j=0;j<crr.length;j++){
+              if(crr[j]==temp){
+                count++;
+                arr[j]=-1;
+              }
+            }
+            if(temp != -1){
+             var subcrr = [temp,count] 
+              newCrr.push(subcrr)
+            }
+          }
+          return newCrr;
+        }    
+        }
+      }
+      window.myhouse = function(results){
+          for (var i = 0; i < results.data.length; i++) {
+            var hrr =  results.data[i];
+        var hrr1= hrr[15];
+        var hrr2= hrr[25];
+        var hrr3= hrr[26];
+        var hrr4= hrr[31]+hrr[32]+hrr[33];
+        var hrr5= hrr[34]+hrr[35];
+        var hrr6= hrr[36];
+        }
+       }
+
+      function initMap() {
+        var uluru = {lat: 40.7291, lng: -73.9965};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 20,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
